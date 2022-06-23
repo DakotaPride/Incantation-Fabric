ed830172febd27768f0f3e57fa825fc08f2f34f3
DakotaPride <rjtube13@gmail.com>
2022-06-22 22:32:49 -0500

    Incantation

--- src/main/java/net/dakotapride/incantation/compat/enhancedcelestials/EnhancedCelestialsCompat.java
+++ src/main/java/net/dakotapride/incantation/compat/enhancedcelestials/EnhancedCelestialsCompat.java
@@ -4,12 +4,23 @@ import net.dakotapride.incantation.common.IncantationMod;
 import net.dakotapride.incantation.common.effect.EmptyStatusEffect;
 import net.dakotapride.incantation.compat.enhancedcelestials.item.HarvestHealingScrollItem;
 import net.dakotapride.incantation.compat.moreweaponry.item.HarmingResistanceScrollItem;
+import net.dakotapride.incantation.mixin.BrewingRecipeRegistryMixin;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -23,6 +34,11 @@ public class EnhancedCelestialsCompat {
     public static Item MENDING_MOON_CREST_FRUIT;
     public static Item MOON_CREST_FRUIT;
 
+    public static Potion HARVEST_HEALING_POTION;
+
+    private static final Identifier GRASS_BLOCK_ID
+            = new Identifier("minecraft", "blocks/grass");
+
     // Registration
 
     public static <T extends Item> T registerItem(String name, T item) {
@@ -30,7 +46,17 @@ public class EnhancedCelestialsCompat {
         return item;
     }
 
+    public static <T extends Potion> T registerPotion(String name, T potion) {
+        Registry.register(Registry.POTION, new Identifier(ENHANCED_CELESTIALS_ID, name), potion);
+        return potion;
+    }
+
     public static void enhancedCelestialsCompatRegistry() {
+
+        HARVEST_HEALING_POTION = registerPotion("harvest_healing",
+                new Potion(new StatusEffectInstance(HARVEST_HEALING, 220, 0)));
+        BrewingRecipeRegistryMixin.invokeRegisterPotionRecipe(Potions.MUNDANE,
+                Items.WHEAT, HARVEST_HEALING_POTION);
 
         Registry.register(Registry.STATUS_EFFECT, new Identifier(ENHANCED_CELESTIALS_ID, "harvest_healing"), HARVEST_HEALING);
         HARVEST_HEALING_SCROLL = registerItem("harvest_healing_scroll",
@@ -47,6 +73,27 @@ public class EnhancedCelestialsCompat {
                                 .saturationModifier(7.0f)
                                 .statusEffect(new StatusEffectInstance(HARVEST_HEALING, 400), 1.0f)
                                 .build())));
+
+        LootTableEvents.MODIFY.register(((resourceManager, manager, id, supplier, setter) -> {
+            if (GRASS_BLOCK_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.015f))
+                        .with(ItemEntry.builder(MOON_CREST_FRUIT))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                supplier.pool(poolBuilder.build());
+            }
+
+            if (GRASS_BLOCK_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.006f))
+                        .with(ItemEntry.builder(MENDING_MOON_CREST_FRUIT))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                supplier.pool(poolBuilder.build());
+            }
+
+        }));
 
     }
 }
--- src/main/java/net/dakotapride/incantation/compat/moreweaponry/MoreWeaponryCompat.java
+++ src/main/java/net/dakotapride/incantation/compat/moreweaponry/MoreWeaponryCompat.java
@@ -4,10 +4,15 @@ package net.dakotapride.incantation.compat.moreweaponry;
 import net.dakotapride.incantation.common.IncantationMod;
 import net.dakotapride.incantation.common.effect.EmptyStatusEffect;
 import net.dakotapride.incantation.compat.moreweaponry.item.HarmingResistanceScrollItem;
+import net.dakotapride.incantation.mixin.BrewingRecipeRegistryMixin;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -19,6 +24,8 @@ public class MoreWeaponryCompat {
     public static StatusEffect HARMING_RESISTANCE = new EmptyStatusEffect(StatusEffectCategory.BENEFICIAL, 0xCEFFF2);
     public static HarmingResistanceScrollItem HARMING_RESISTANCE_SCROLL;
 
+    public static Potion HARMING_RESISTANCE_POTION;
+
     // Registration
 
     public static <T extends Item> T registerItem(String name, T item) {
@@ -26,8 +33,18 @@ public class MoreWeaponryCompat {
         return item;
     }
 
+    public static <T extends Potion> T registerPotion(String name, T potion) {
+        Registry.register(Registry.POTION, new Identifier(MORE_WEAPONRY_ID, name), potion);
+        return potion;
+    }
+
 
     public static void moreWeaponryCompatRegistry() {
+
+        HARMING_RESISTANCE_POTION = registerPotion("harming_resistance",
+                new Potion(new StatusEffectInstance(HARMING_RESISTANCE, 220, 0)));
+        BrewingRecipeRegistryMixin.invokeRegisterPotionRecipe(Potions.MUNDANE,
+                Items.SPIDER_EYE, HARMING_RESISTANCE_POTION);
 
         Registry.register(Registry.STATUS_EFFECT, new Identifier(MORE_WEAPONRY_ID, "harming_resistance"), HARMING_RESISTANCE);
         HARMING_RESISTANCE_SCROLL = registerItem("harming_resistance_scroll",
