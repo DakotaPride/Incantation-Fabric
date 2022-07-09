5240e4a2536134e9a0803093bc364c62f8d4305c
DakotaPride <rjtube13@gmail.com>
2022-07-08 21:20:13 -0500

    Incantation

--- src/main/java/net/dakotapride/incantation/common/util/IncantationTags.java
+++ src/main/java/net/dakotapride/incantation/common/util/IncantationTags.java
@@ -10,13 +10,13 @@ import net.minecraft.util.registry.Registry;
 public class IncantationTags {
 
         public static final TagKey<Item> HARVESTABLE_FOODS = createCommonItemTag("harvestable_healing_foods");
-        public static final TagKey<StatusEffect> PICK_YOUR_POISON_EFFECTS = createStatusEffectTag("pick_your_poison_effects");
+        public static final TagKey<StatusEffect> PICK_YOUR_POISON_EFFECTS = createPickYourPoisonStatusEffectTag("pick_your_poison_effects");
 
         private static TagKey<Item> createCommonItemTag(String name) {
             return TagKey.of(Registry.ITEM_KEY, new Identifier("c", name));
         }
 
-        private static TagKey<StatusEffect> createStatusEffectTag(String name) {
+        private static TagKey<StatusEffect> createPickYourPoisonStatusEffectTag(String name) {
             return TagKey.of(Registry.MOB_EFFECT_KEY, new Identifier(PickYourPoisonCompat.PICK_YOUR_POISON_ID, name));
         }
 
