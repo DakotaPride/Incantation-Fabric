cc8f9fe86f54478ec8aaf60a60c4ba4083aef139
DakotaPride <rjtube13@gmail.com>
2022-07-16 13:08:57 -0500

    Incantation v0.1.4

--- src/main/java/net/dakotapride/incantation/common/item/EffectScrollItem.java
+++ src/main/java/net/dakotapride/incantation/common/item/EffectScrollItem.java
@@ -1,10 +1,17 @@
 package net.dakotapride.incantation.common.item;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
 
 public class EffectScrollItem extends Item {
+    private final ToolMaterial material;
     public EffectScrollItem(ToolMaterial material, Settings settings) {
         super(settings);
+        this.material = material;
+    }
+
+    public boolean canRepair(ItemStack stack, ItemStack ingredient) {
+        return (this.material.getRepairIngredient().test(ingredient) || super.canRepair(stack, ingredient));
     }
 }
