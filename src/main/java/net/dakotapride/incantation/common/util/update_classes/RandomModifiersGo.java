1fdcc9ca73bd5008974e1cb225a0b14322763473
DakotaPride <rjtube13@gmail.com>
2022-07-29 11:35:18 -0500

    Added Recipes (Doughs, Cinnamon Buns, Modifier Packs)

--- src/main/java/net/dakotapride/incantation/common/util/update_classes/RandomModifiersGo.java
+++ src/main/java/net/dakotapride/incantation/common/util/update_classes/RandomModifiersGo.java
@@ -1,10 +1,8 @@
 package net.dakotapride.incantation.common.util.update_classes;
 
 import net.dakotapride.incantation.common.item.EnchantedItem;
-import net.dakotapride.incantation.common.item.modifiers.items_modified.GTUnconcealedBleakScroll;
-import net.dakotapride.incantation.common.item.modifiers.items_modified.SweetUnconcealedBleakScroll;
-import net.dakotapride.incantation.common.item.modifiers.packs.GoldenTouchModifierItemPack;
-import net.dakotapride.incantation.common.item.modifiers.packs.SweetenedModifierItemPack;
+import net.dakotapride.incantation.common.item.modifiers.items_modified.*;
+import net.dakotapride.incantation.common.item.modifiers.packs.*;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
@@ -20,11 +18,15 @@ public class RandomModifiersGo {
     public static EnchantedItem ENCHANTED_CINNAMON_BUN;
     public static Item GOLDEN_CINNAMON_BUN;
     public static EnchantedItem ENCHANTED_GOLDEN_CINNAMON_BUN;
+    public static Item DOUGH;
+    public static Item GOLDEN_DOUGH;
     public static SweetenedModifierItemPack SWEETENED_MODIFIER_ITEM_PACK;
     public static GoldenTouchModifierItemPack GOLDEN_TOUCH_MODIFIER_ITEM_PACK;
     public static Item EMPTY_MODIFIER_PACK;
     public static GTUnconcealedBleakScroll gtUNCONCEALED_BLEAK_SCROLL;
     public static SweetUnconcealedBleakScroll sweetUNCONCEALED_BLEAK_SCROLL;
+    public static GTUnconcealedRaWrathScroll gtUNCONCEALED_RA_WRATH_SCROLL;
+    public static SweetUnconcealedRaWrathScroll sweetUNCONCEALED_RA_WRATH_SCROLL;
 
     public static <T extends Item> T registerItem(String name, T item) {
         Registry.register(Registry.ITEM, new Identifier(INCANTATION_ID, name), item);
@@ -33,11 +35,18 @@ public class RandomModifiersGo {
 
     public static void registerRandomModifiersGoFeatures() {
         gtUNCONCEALED_BLEAK_SCROLL = registerItem("unconcealed_bleak_scroll_gt",
-                new GTUnconcealedBleakScroll(IncantationMaterials.FABRIC_PATCHING,
-                        new FabricItemSettings().group(INCANTATION_GROUP)));
+                new GTUnconcealedBleakScroll(IncantationMaterials.PACKED_FABRIC_PATCHING,
+                        new FabricItemSettings().maxDamage(80).group(INCANTATION_GROUP)));
         sweetUNCONCEALED_BLEAK_SCROLL = registerItem("unconcealed_bleak_scroll_sweet",
-                new SweetUnconcealedBleakScroll(IncantationMaterials.FABRIC_PATCHING,
-                        new FabricItemSettings().group(INCANTATION_GROUP)));
+                new SweetUnconcealedBleakScroll(IncantationMaterials.PACKED_FABRIC_PATCHING,
+                        new FabricItemSettings().maxDamage(80).group(INCANTATION_GROUP)));
+
+        gtUNCONCEALED_RA_WRATH_SCROLL = registerItem("unconcealed_ra_wrath_scroll_gt",
+                new GTUnconcealedRaWrathScroll(IncantationMaterials.PACKED_FABRIC_PATCHING,
+                        new FabricItemSettings().maxDamage(80).group(INCANTATION_GROUP)));
+        sweetUNCONCEALED_RA_WRATH_SCROLL = registerItem("unconcealed_ra_wrath_scroll_sweet",
+                new SweetUnconcealedRaWrathScroll(IncantationMaterials.PACKED_FABRIC_PATCHING,
+                        new FabricItemSettings().maxDamage(80).group(INCANTATION_GROUP)));
 
         CINNAMON_BUN = registerItem("cinnamon_bun",
                 new Item(new FabricItemSettings().food(new FoodComponent.Builder().saturationModifier(1.0f).hunger((int) 1.0f)
@@ -51,6 +60,10 @@ public class RandomModifiersGo {
         ENCHANTED_GOLDEN_CINNAMON_BUN = registerItem("enchanted_golden_cinnamon_bun",
                 new EnchantedItem(new FabricItemSettings().food(new FoodComponent.Builder().saturationModifier(3.0f).hunger((int) 4.0f)
                         .build()).group(INCANTATION_GROUP)));
+        DOUGH = registerItem("dough",
+                new Item(new FabricItemSettings()));
+        GOLDEN_DOUGH = registerItem("golden_dough",
+                new Item(new FabricItemSettings()));
 
         SWEETENED_MODIFIER_ITEM_PACK = registerItem("sweetened_modifier_pack",
                 new SweetenedModifierItemPack(new FabricItemSettings().rarity(Rarity.UNCOMMON).group(INCANTATION_GROUP)));
